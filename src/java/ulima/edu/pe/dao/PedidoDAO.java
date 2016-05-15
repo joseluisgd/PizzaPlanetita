33736b30f1b1e5e09a412118e4e0747360e8492e
Christopher Fernández <Christopher Fernndez>
2016-05-15 18:50:46 -0500

    Ya se registran los pedidos

--- src/java/ulima/edu/pe/dao/PedidoDAO.java
+++ src/java/ulima/edu/pe/dao/PedidoDAO.java
@@ -1,4 +1,3 @@
-
 package ulima.edu.pe.dao;
 
 import com.mongodb.BasicDBObject;
@@ -7,52 +6,68 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import java.util.ArrayList;
 import java.util.List;
 import ulima.edu.pe.beans.Estado;
+import ulima.edu.pe.beans.Ingrediente;
 import ulima.edu.pe.beans.Pizza;
 import ulima.edu.pe.beans.Producto;
 import ulima.edu.pe.util.ConexionMLab;
 
-
 public class PedidoDAO {
-    public void agregar(Estado estado,String usuario, String direccion, List<Pizza> pizzas, List<Producto> productos) {
+
+    public void agregar(Estado estado, String usuario, String direccion, List<Pizza> pizzas, List<Producto> productos) {
         ConexionMLab con = new ConexionMLab();
         MongoClient mongo = con.getConexion();
         try {
             DB db = mongo.getDB("basededatos");
             DBCollection coleccion = db.getCollection("pedido");
-            
+
             BasicDBObject doc = new BasicDBObject();
-            
+
             doc.put("id", contar() + 1);
-            
-            
+
             BasicDBObject doc1 = new BasicDBObject();
             doc1.put("fechahora", estado.getHora());
             doc1.put("id", estado.getId());
             doc1.put("estado", estado.getEstado());
-            
+
             doc.put("Estado", doc1);
-            
-            
+
             doc.put("usu", usuario);
-            
+
             doc.put("direccion", direccion);
-            
-            BasicDBObject doc2 = new BasicDBObject();
+
+            BasicDBObject doc2;
+            BasicDBObject doc4;
+            ArrayList arrayPizzas = new ArrayList();
+            ArrayList arrayIngredientes = new ArrayList();
+            List<Ingrediente> ingredientes;
             for (Pizza pizza : pizzas) {
-                doc2.put("ingredientes", pizza.getIng());
+                doc2 = new BasicDBObject();
+                ingredientes = pizza.getIng();
+                for (Ingrediente ingrediente : ingredientes) {
+                    doc4 = new BasicDBObject();
+                    doc4.put("id", ingrediente.getId());
+                    doc4.put("nombre", ingrediente.getNombre());
+                    arrayIngredientes.add(doc4);
+                }
+                doc2.put("Ingredientes", arrayIngredientes);
                 doc2.put("precio", pizza.getPrecio());
-            } 
-            doc.put("Pizzas", doc2);
-            
-            BasicDBObject doc3 = new BasicDBObject();
+                arrayPizzas.add(doc2);
+            }
+            doc.put("Pizzas", arrayPizzas);
+
+            BasicDBObject doc3;
+            ArrayList arrayProductos = new ArrayList();
             for (Producto producto : productos) {
+                doc3 = new BasicDBObject();
                 doc3.put("id", producto.getId());
                 doc3.put("nombre", producto.getNombre());
                 doc3.put("precio", producto.getPrecio());
+                arrayProductos.add(doc3);
             }
-            doc.put("Productos", doc3);
+            doc.put("Productos", arrayProductos);
 
             coleccion.insert(doc);
         } catch (Exception e) {
@@ -61,7 +76,7 @@ public class PedidoDAO {
             mongo.close();
         }
     }
-    
+
     private Integer contar() {
         ConexionMLab con = new ConexionMLab();
         MongoClient mongo = con.getConexion();
