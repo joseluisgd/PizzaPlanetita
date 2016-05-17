ced8214b8dd9c2e4fde2f402a6703b9fe3bcfbf8
christopherfr <chfernandezrios@gmail.com>
2016-05-17 14:25:09 -0500

    Función obtenerFechaHoraActual() añadida.

--- src/java/ulima/edu/pe/util/Util.java
+++ src/java/ulima/edu/pe/util/Util.java
@@ -1,6 +1,10 @@
 package ulima.edu.pe.util;
 
-// No sé si compila, verificar
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+// Verificar si compila
 public class Util {
 	private Util singleton = null;
 	
@@ -14,8 +18,12 @@ public class Util {
 	}
 	
 	public static String obtenerFechaHoraActual() {
-		Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT-05:00"));
-		// Incompleto
+		// Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT-05:00")); // No funciona
+		// c.getTime(); // Sigue saliendo la hora en GMT-00:00.
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.HOUR_OF_DAY, -5);
+		
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		return df.format(c.getTime());
 	}
-	
 }
