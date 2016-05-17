4dbd9807b719ce3ea5c1cabd49b158d97b34e938
christopherfr <chfernandezrios@gmail.com>
2016-05-17 10:31:55 -0500

    Huevaditas
    - Eliminación de comentarios autogenerados.
    - Cambio de nombre de variables dao por uno más descriptivo.

--- src/java/ulima/edu/pe/servlet/ServletIngrediente.java
+++ src/java/ulima/edu/pe/servlet/ServletIngrediente.java
@@ -20,51 +20,28 @@ public class ServletIngrediente extends HttpServlet {
 
         HttpSession ses = request.getSession(true);
 
-        IngredienteDAO dao = new IngredienteDAO();
-        ProductoDAO dao1= new ProductoDAO();
-        ses.setAttribute("ingredientes", dao.getIngredientes());
-        ses.setAttribute("productos", dao1.getProductos());
+        IngredienteDAO daoIngrediente = new IngredienteDAO();
+        ses.setAttribute("ingredientes", daoIngrediente.getIngredientes());
+        
+        ProductoDAO daoProducto = new ProductoDAO();
+        ses.setAttribute("productos", daoProducto.getProductos());
 
         RequestDispatcher rd = request.getRequestDispatcher("crearPedido.jsp");
-
         rd.forward(request, response);
-
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
