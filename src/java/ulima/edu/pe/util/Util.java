9b6fd7f96fe84cde95692ce01379d46333ccd71e
christopherfr <chfernandezrios@gmail.com>
2016-05-17 14:04:48 -0500

    Update Util.java

--- src/java/ulima/edu/pe/util/Util.java
+++ src/java/ulima/edu/pe/util/Util.java
@@ -1,5 +1,21 @@
 package ulima.edu.pe.util;
 
+// No sé si compila, verificar
 public class Util {
-
+	private Util singleton = null;
+	
+	private Util {}
+	
+	public static Util getInstance() {
+		if (singleton == null) {
+			singleton = new Util;
+		}
+		return singleton;
+	}
+	
+	public static String obtenerFechaHoraActual() {
+		Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT-05:00"));
+		// Incompleto
+	}
+	
 }
