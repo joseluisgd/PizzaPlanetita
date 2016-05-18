011624642699371092841a99ac80dc8a727adbc2
christopherfr <chfernandezrios@gmail.com>
2016-05-17 19:10:53 -0500

    Update ServletIngrediente.java

--- src/java/ulima/edu/pe/servlet/ServletIngrediente.java
+++ src/java/ulima/edu/pe/servlet/ServletIngrediente.java
@@ -1,15 +1,12 @@
 package ulima.edu.pe.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import ulima.edu.pe.beans.Ingrediente;
 import ulima.edu.pe.dao.IngredienteDAO;
 import ulima.edu.pe.dao.ProductoDAO;
 
