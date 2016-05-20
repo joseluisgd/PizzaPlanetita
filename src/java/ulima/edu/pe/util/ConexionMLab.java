d0a8add7e19efe30b548091ec781e117dc311736
christopherfr <chfernandezrios@gmail.com>
2016-05-20 15:18:27 -0500

    Agregado como comentario la conexión a la bd de pruebas

--- src/java/ulima/edu/pe/util/ConexionMLab.java
+++ src/java/ulima/edu/pe/util/ConexionMLab.java
@@ -9,11 +9,15 @@ public class ConexionMLab {
     private final String username = "grupo01";
     private final String password = "progra";
     private final String database = "basededatos";
+    //private final String username = "pizza";
+    //private final String password = "pizza";
+    //private final String database = "pizzaplanetatest";
     private MongoClient cliente;
     private MongoClientURI uri;
 
     public ConexionMLab() {
         uri = new MongoClientURI("mongodb://" + username + ":" + password + "@ds063124.mlab.com:63124/" + database);
+        //uri = new MongoClientURI("mongodb://" + username + ":" + password + "@ds025742.mlab.com:25742/" + database);
         try {
             cliente = new MongoClient(uri);
         } catch (UnknownHostException ex) {
