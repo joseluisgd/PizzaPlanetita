18693f3ba8b3d2afc12fae0cb4bb1e0cebe5452c
christopherfr <chfernandezrios@gmail.com>
2016-05-20 15:59:58 -0500

    Update ConexionMLab.java
    - Código comentado con cambios de código, si funciona, se podría usar una línea en vez de dos para poder llamar al cliente en los DAOs.
    - Quizás el singleton sí funcionaba y era cuestión de cambiar un par de cosas. Averiguar.

--- src/java/ulima/edu/pe/util/ConexionMLab.java
+++ src/java/ulima/edu/pe/util/ConexionMLab.java
@@ -29,3 +29,34 @@ public class ConexionMLab {
         return cliente;
     }
 }
+
+/*
+package ulima.edu.pe.util;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import java.net.UnknownHostException;
+
+public class ConexionMLab {
+    
+    //probar funcionamiento urgente
+    private static final String username = "grupo01";
+    private static final String password = "progra";
+    private static final String database = "basededatos";
+    //private static final String username = "pizza";
+    //private static final String password = "pizza";
+    //private static final String database = "pizzaplanetatest";
+    private static final MongoClientURI uri = new MongoClientURI("mongodb://" + username + ":" + password + "@ds063124.mlab.com:63124/" + database);
+    //private static final MongoClientURI uri = new MongoClientURI("mongodb://" + username + ":" + password + "@ds025742.mlab.com:25742/" + database);
+    private MongoClient cliente;
+
+    public MongoClient getConexion() {
+        try {
+            cliente = new MongoClient(uri);
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
+        }
+        return cliente;
+    }
+}
+*/
